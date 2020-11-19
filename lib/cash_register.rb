3a196dbfb16c4d70b7c65fd9fb30457baa6d861d
racross1 <racross1@gmail.com>
2020-11-18 21:08:37 -0800

    removed some extra comments

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,23 +32,6 @@ class CashRegister
     end 
 
 
-#     describe '#void_last_transaction' do
-#     it 'subtracts the last item from the total' do
-#       cash_register.add_item("apple", 0.99)
-#       cash_register.add_item("tomato", 1.76)
-#       cash_register.void_last_transaction
-#       expect(cash_register.total).to eq(0.99)
-#     end
-
-#     it 'returns the total to 0.0 if all items have been removed' do
-#       cash_register.add_item("tomato", 1.76, 2)
-#       expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(3.52).to(0.0)
-#     end
-#   end
-# end    
-
-
-
 
     def apply_discount
         #binding.pry 
